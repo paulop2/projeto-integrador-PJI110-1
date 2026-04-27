57264e212cc0ff7970be34ef2733a08fab6fcb0c
paulop2 <paulo225vitor@gmail.com>
2026-04-27 18:20:20 -0300

    feat(04-01): create Chamada, Presenca, Avaliacao, Nota ORM models

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -5,9 +5,14 @@ from src.models.disciplina import Disciplina
 from src.models.professor import Professor
 from src.models.responsavel import Responsavel
 from src.models.professor_turma import ProfessorTurma
+from src.models.chamada import Chamada
+from src.models.presenca import Presenca
+from src.models.avaliacao import Avaliacao
+from src.models.nota import Nota
 
 __all__ = [
     "Usuario", "ResetToken", "TipoUsuario",
     "Aluno", "Turma", "Disciplina",
     "Professor", "Responsavel", "ProfessorTurma",
+    "Chamada", "Presenca", "Avaliacao", "Nota",
 ]
